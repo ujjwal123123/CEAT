a41573b7c16bdb5a3de55b86c1f51cd26da06f57
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-07-02 02:35:06 +0530

    Many changes

--- program/ceat.py
+++ program/ceat.py
@@ -5,7 +5,7 @@ import sys
 from collections import defaultdict
 from typing import DefaultDict, List, Optional, Set, Tuple
 
-TOTAL_TIME = 100000
+TOTAL_TIME = 10000
 
 
 class Core:
@@ -20,6 +20,9 @@ class Core:
     def __str__(self) -> str:
         return f"{self.core_id}"
 
+    def __hash__(self) -> int:
+        return hash(self.core_id)
+
 
 class Task:
     def __init__(
@@ -27,22 +30,26 @@ class Task:
     ):
         self.id = id
         self.period: int = period
+        self.remaining_period: float = period
         self.exec: float = execution_requirement  # execution requirement
         self.rates: List[float] = rates
         self.core_count = len(rates)
-        self.__utilizations: Optional[List[float]] = None
 
     def __repr__(self) -> str:
         return f"{self.id}.\tPeriod: {self.period}\tExecution Requirement: {self.exec}"
 
     def get_utilizations(self) -> List[float]:
-        if self.__utilizations is None:
-            self.__utilizations = [self.exec * ratio for ratio in self.ratios]
-        return self.__utilizations
+        return [self.exec / (self.period * rate) for rate in self.rates]
+
+    def get_utilization(self, core: Core) -> float:
+        return self.get_utilizations()[core.core_id]
+
+    def get_share(self, core: Core, frame_length: float):
+        return (self.exec * frame_length) / (self.period * self.rates[core.core_id])
 
 
 class Cluster:
-    def __init__(self, first_core: Core, second_core: Core):
+    def __init__(self, first_core: Core, second_core: Core, frame_length: float):
         assert first_core != second_core
         if first_core.core_id < second_core.core_id:
             self.first_core = first_core
@@ -50,6 +57,7 @@ class Cluster:
         else:
             self.first_core = second_core
             self.second_core = first_core
+        self.spare_capacity: Optional[float]
         self.tasks_allocated: Set[Task] = set()
 
     def __eq__(self, o: object) -> bool:
@@ -64,6 +72,9 @@ class Cluster:
 
     def __hash__(self) -> int:
         return hash((self.first_core.core_id, self.second_core.core_id))
+
+    def set_spare_capacity(self, spare_capacity: float):
+        self.spare_capacity = spare_capacity
 
 
 class FileReader:
@@ -100,10 +111,12 @@ class FileReader:
 
 class Scheduler:
     @staticmethod
-    def task_with_lowest_requirement(
-        tasks: List[Task], allocated_tasks: List[Task]
-    ) -> Tuple[Task, Core]:
-        """Returns the task with lowest execution requirement along with the core on
+    def task_with_lowest_share(
+        tasks: List[Task],
+        allocated_tasks: List[Task],
+        task_core_considered: DefaultDict[Task, Set[Core]],
+    ) -> Optional[Tuple[Task, Core]]:
+        """Returns the task with lowest share value along with the core on
         which it should be executed"""
         lowest_requirement: Optional[float] = None
         task_lowest: Optional[Task] = None  # task with the lowest requirement
@@ -113,16 +126,23 @@ class Scheduler:
 
         for task in tasks:
             if task not in allocated_tasks:
-                utilizations: List[float] = task.get_utilizations()
-                if lowest_requirement is None or min(utilizations) < lowest_requirement:
-                    lowest_requirement = min(utilizations)
-                    task_lowest = task
-                    core_lowest = Core(utilizations.index(lowest_requirement))
+                for core_id in range(task.core_count):
+                    core = Core(core_id)
 
-        assert task_lowest is not None
-        assert core_lowest is not None
+                    if core in task_core_considered[task]:
+                        continue
+                    utilization: float = task.get_utilization(core)
+                    if lowest_requirement is None or utilization < lowest_requirement:
+                        lowest_requirement = utilization
+                        task_lowest = task
+                        core_lowest = core
 
-        return (task_lowest, core_lowest)
+        if core_lowest:
+            assert task_lowest is not None
+            return (task_lowest, core_lowest)
+        else:
+            print("Task could not be assigned")
+            return None
 
     @staticmethod
     def find_second_core(
@@ -136,12 +156,13 @@ class Scheduler:
         min_value: Optional[float] = None
         min_core: Optional[
             Core
-        ] = None  # core associated with second lowest requirement value
+        ] = None  # core associated with second lowest utilization
 
         for core_index, requirement in enumerate(task.get_utilizations()):
-            if Core(core_index) not in allocated_cores:
+            core: Core = Core(core_index)
+            if core not in allocated_cores:
                 if min_value is None or (lowest_value < requirement < min_value):
-                    min_core = Core(core_index)
+                    min_core = core
                     min_value = requirement
 
         assert min_core is not None
@@ -156,50 +177,95 @@ class Scheduler:
 
         raise Exception("Not found")
 
-    def ceat(self, tasks: List[Task]):
-        clusters: Set[Cluster] = self.construct_clusters(tasks)
-        for index, cluster in enumerate(clusters):
-            print(
-                f"Cluster {index}: cores {cluster.first_core} and {cluster.second_core}"
-            )
-            for task in cluster.tasks_allocated:
-                print(task)
-            print()
+    @staticmethod
+    def average_computation_demand(
+        task: Task, first_core: Core, second_core: Core, frame_length: float
+    ) -> float:
+        # TODO: must I use get_share() here?
+        return (
+            task.get_share(first_core, frame_length)
+            + task.get_share(second_core, frame_length)
+        ) / 2
 
-    def construct_clusters(self, tasks: List[Task]) -> Set[Cluster]:
+    def ceat(self, tasks: List[Task]):
+        time = 0
+        while time < TOTAL_TIME:
+            print("running")
+            frame_length: float = min([task.remaining_period for task in tasks])
+            clusters: Set[Cluster] = self.construct_clusters(tasks, frame_length)
+
+            for index, cluster in enumerate(clusters):
+                print(
+                    f"Cluster {index}: cores {cluster.first_core} and {cluster.second_core}"
+                )
+                for task in cluster.tasks_allocated:
+                    print(task)
+                print()
+
+            for task in tasks:
+                task.remaining_period -= frame_length
+            time += frame_length
+
+    def construct_clusters(
+        self, tasks: List[Task], frame_length: float
+    ) -> Set[Cluster]:
         tasks_allocated: List[Task] = []
         cores_allocated: List[Core] = []
 
         clusters: Set[Cluster] = set()
+        for cluster in clusters:
+            cluster.set_spare_capacity(2 * frame_length)
+
+        tasks_cores_considered: DefaultDict[Task, Set[Core]] = defaultdict(set)
 
         while len(tasks_allocated) < len(tasks):
-            task, first_core = Scheduler.task_with_lowest_requirement(
-                tasks, tasks_allocated
+            ret = Scheduler.task_with_lowest_share(
+                tasks, tasks_allocated, tasks_cores_considered
             )
-
-            if task is None or first_core is None:
+            if ret is None:
                 exit("Infeasible task set")
+            task, first_core = ret
+
+            tasks_cores_considered[task].add(first_core)
 
             if first_core not in cores_allocated:
                 cores_allocated.append(first_core)
                 second_core: Core = Scheduler.find_second_core(
                     tasks, task, cores_allocated
                 )
+                computation_demand = Scheduler.average_computation_demand(
+                    task, first_core, second_core, frame_length
+                )
+                tasks_cores_considered[task].add(second_core)
+                if computation_demand < frame_length:
+                    new_cluster = Cluster(first_core, second_core, frame_length)
+                    new_cluster.set_spare_capacity(2 * frame_length)
+                    new_cluster.tasks_allocated.add(task)
 
-                new_cluster = Cluster(first_core, second_core)
-                new_cluster.tasks_allocated.add(task)
+                    cores_allocated.append(second_core)
+                    clusters.add(new_cluster)
+                    tasks_allocated.append(task)
 
-                cores_allocated.append(second_core)
-                clusters.add(new_cluster)
+                    assert new_cluster.spare_capacity is not None
+                    new_cluster.spare_capacity -= computation_demand
 
             else:
                 cluster: Cluster = Scheduler.find_cluster_with_core(
                     clusters, first_core
                 )
 
-                cluster.tasks_allocated.add(task)
+                tasks_cores_considered[task].add(cluster.first_core)
+                tasks_cores_considered[task].add(cluster.second_core)
 
-            tasks_allocated.append(task)
+                computation_demand: float = Scheduler.average_computation_demand(
+                    task, cluster.first_core, cluster.second_core, frame_length
+                )
+                # TODO: following is the buggy line
+                assert cluster.spare_capacity is not None
+                if computation_demand <= cluster.spare_capacity + 100:
+                    cluster.tasks_allocated.add(task)
+                    tasks_allocated.append(task)
+                    cluster.spare_capacity -= computation_demand
 
         return clusters
 
